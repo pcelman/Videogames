5de7d71f15b2a92843ec0aed6e1d38b7c0cef5be
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-11-22 14:12:59 -0300

    home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,116 +1,119 @@
 import React from "react";
-import {useState, useEffect} from "react"
-import  { useDispatch, useSelector } from "react-redux";
-import { getVideogames, getGenre, savePage, getPlatforms } from "../actions/index.js"
-import { Link } from "react-router-dom"
-import  Card  from "./Card"
-import  Paginado  from "./Paginado"
-import gif from "./loading.gif"
-import jpg from "./placeHolder.jpg"
-import notF from "./notF.png"
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getVideogames,
+  getGenre,
+  savePage,
+  getPlatforms,
+} from "../actions/index.js";
+import { Link } from "react-router-dom";
+import Card from "./Card";
+import Paginado from "./Paginado";
+import gif from "./loading.gif";
+import jpg from "./placeHolder.jpg";
+import notF from "./notF.png";
 
-
-import "../styles/home.css"
+import "../styles/home.css";
 import NavBar from "./NavBar.jsx";
 
+export default function Home() {
+  const dispatch = useDispatch();
+  const videogamesFilter = useSelector((state) => state.videogamesFilter);
+  const allVideogames = useSelector((state) => state.videogames);
+  const status = useSelector((state) => state.status);
+  const pages1 = useSelector((state) => state.pages);
+  const platforms = useSelector((state) => state.platforms);
 
-export default function Home(){
+  const videogames = useSelector((state) => state.videogames);
+  //paginado
 
-    const dispatch = useDispatch()
-    const videogamesFilter = useSelector ((state) => state.videogamesFilter)
-    const allVideogames = useSelector((state) => state.videogames)
-    const status = useSelector ((state) => state.status)
-    const pages1 = useSelector((state)=>state.pages)
-    const platforms = useSelector((state) => state.platforms)
-    
+  const [order, setOrder] = useState("");
+  const [currentPage, setCurrentPage] = useState(pages1);
+  const [videogamePerPage, setVideogamesPerPage] = useState(12);
+  const indexOfLastVideogame = currentPage * videogamePerPage;
+  const indexOfFirstVideogame = indexOfLastVideogame - videogamePerPage;
+  const currentVideogames = videogamesFilter.slice(
+    indexOfFirstVideogame,
+    indexOfLastVideogame
+  );
 
-    const videogames = useSelector ((state) => state.videogames)
-    //paginado
+  const paginado = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-    const [order, setOrder] = useState("")
-    const[currentPage, setCurrentPage] = useState(pages1)
-    const[videogamePerPage, setVideogamesPerPage] = useState(12)
-    const indexOfLastVideogame = currentPage * videogamePerPage 
-    const indexOfFirstVideogame = indexOfLastVideogame - videogamePerPage
-    const currentVideogames = videogamesFilter.slice(indexOfFirstVideogame, indexOfLastVideogame)
+  useEffect(() => {
+    dispatch(getGenre());
+    dispatch(getVideogames());
+    dispatch(getPlatforms());
+  }, [dispatch]);
 
-    const paginado = (pageNumber) => {
-        setCurrentPage(pageNumber)
-    }
+  function handlePage(e) {
+    dispatch(savePage(currentPage));
+  }
 
-    useEffect(()=>{
-        dispatch(getGenre())
-        dispatch(getVideogames())
-        dispatch(getPlatforms())
-    },[dispatch]) 
-
-    function handlePage(e){
-        dispatch(savePage(currentPage))
-    }
-
-    return ( 
-    
-        <div className="container-home">
-    <div className="titulo-home">
+  return (
+    <div className="container-home">
+      <div className="titulo-home">
         <div className="videogames-home">VIDEOGAMES</div>
-        <Link to = "/create"> <button className="boton-home">CREATE</button></Link>
-        {/* <button onClick={e=> {handleClick(e)}}>RELOAD</button> */}
-         <NavBar setCurrentPage={setCurrentPage} setOrder={setOrder} />  
-    </div>
+        <Link to="/create">
+          {" "}
+          <button className="boton-home">CREATE</button>
+        </Link>
 
-<div className="paginado">
+        <NavBar setCurrentPage={setCurrentPage} setOrder={setOrder} />
+      </div>
 
-<Paginado
-videogamePerPage={videogamePerPage}
-videogamesFilter={videogamesFilter.length}
-paginado={paginado}
-currentPage={currentPage}
-/>
-</div>
+      <div className="paginado">
+        <Paginado
+          videogamePerPage={videogamePerPage}
+          videogamesFilter={videogamesFilter.length}
+          paginado={paginado}
+          currentPage={currentPage}
+        />
+      </div>
 
-     
-<div className="div-cards">
-
-            { videogamesFilter[0] === "otra cosa" ? 
-            <  img src = {notF} alt= "not found " width="610px"   />
-            :  
-            // videogamesFilter.length === 0 ? 
-            // <  img src = {notF} alt= "not found " width="610px"   />:
-            currentVideogames?.map((e) =>{ 
+      <div className="div-cards">
+        {videogamesFilter[0] === "otra cosa" ? (
+          <img src={notF} alt="not found " width="610px" />
+        ) : (
+          currentVideogames?.map((e) => {
             return (
-                <div className="tarjeta-home" 
-                // onHover={handleHover}
+              <div className="tarjeta-home">
+                <Link
+                  className="link"
+                  onClick={(e) => handlePage(e)}
+                  to={`/detail/${e.id}`}
                 >
-                 
-                    <Link className="link"onClick={(e)=>handlePage(e)} to ={`/detail/${e.id}`}> 
-                        <Card onMouseOver={{scale: 1.4}} key= {e.id} name = {e.name} image = {e.image? e.image : <img src = {jpg} width="310px" height="200px" />} genre= {e.genres.map((e)=>e.name)} rating= {e.rating}/>
-                    </Link>
-                 
-                 </div>
-           );
-        })}      
-</div>
-        {videogamesFilter.length === 0 && allVideogames.length === 0 && <  img src = {gif} alt= "Loading... " width="610px"  />}
-        {
-         (allVideogames.length > 0 && videogamesFilter.length === 0 )? 
-         <  img src = {notF} alt= "not found " width="610px"   />
-        :
-        <span></span>}
-        
-        <div className="paginado">
-
-           
-            {/* <Paginado 
-            
-videogamePerPage={videogamePerPage}
-videogamesFilter={videogamesFilter.length}
-paginado={paginado}
-currentPage={currentPage}
-/> */}
-        </div>
-
-</div>
-    )
+                  <Card
+                    onMouseOver={{ scale: 1.4 }}
+                    key={e.id}
+                    name={e.name}
+                    image={
+                      e.image ? (
+                        e.image
+                      ) : (
+                        <img src={jpg} width="310px" height="200px" />
+                      )
+                    }
+                    genre={e.genres.map((e) => e.name)}
+                    rating={e.rating}
+                  />
+                </Link>
+              </div>
+            );
+          })
+        )}
+      </div>
+      {videogamesFilter.length === 0 && allVideogames.length === 0 && (
+        <img src={gif} alt="Loading... " width="610px" />
+      )}
+      {allVideogames.length > 0 && videogamesFilter.length === 0 ? (
+        <img src={notF} alt="not found " width="610px" />
+      ) : (
+        <span></span>
+      )}
+      <div className="paginado"></div>
+    </div>
+  );
 }
-
-
