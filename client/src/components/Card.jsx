194db566b486f7e1cc69f59cafa2fd029610f5e2
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-11-23 18:09:27 -0300

    card css

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -10,17 +10,18 @@ export default function Card({ name, image, genre, rating }) {
   return (
     <body className="card">
       <img src={image} alt={name} className="card__picture" />
-    <section className="card__text">
-
-      <div className="card__title">{name}</div>
-      {/* <div className="card__box"></div> */}
-
       <div className="card__stars">
         {[...Array(Math.floor(rating))].map((i) => (
             <div key={i}>⭐</div>
             ))}
       </div>
-      <ul className="card__genre">{genre.join(", ")}</ul>
+      
+    <section className="card__text">
+
+      <div className="card__title">{name}</div>
+      {/* <div className="card__box"></div> */}
+
+      {/* <ul className="card__genre">{genre.join(", ")}</ul> */}
 
             </section>
     </body>
