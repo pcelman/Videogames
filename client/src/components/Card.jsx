f5b96b40a9e21b14099d4a1cd37ebf3cfac3f475
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-11-22 22:40:37 -0300

    position

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,40 +1,28 @@
 import React from "react";
-import '../styles/card.css';
-import fStar from "./star-full-icon.png"
+import "../styles/card.css";
+import fStar from "./star-full-icon.png";
 
+export default function Card({ name, image, genre, rating }) {
+  // const star= <img src ={fStar}/>
+  const star = "*";
+  const index = Math.floor(rating);
 
+  return (
+    <body className="card">
+      <img src={image} alt={name} className="card__picture" />
+    <section className="card__text">
 
-export default function Card({ name, image, genre, rating }){
-// const star= <img src ={fStar}/>
-const star= "*"
-const index = Math.floor(rating)
+      <div className="card__title">{name}</div>
+      {/* <div className="card__box"></div> */}
 
+      <div className="card__stars">
+        {[...Array(Math.floor(rating))].map((i) => (
+            <div key={i}>⭐</div>
+            ))}
+      </div>
+      <ul className="card__genre">{genre.join(", ")}</ul>
 
-    return(      
-            
-                <body className="card" > 
-                
-                <img src={image} alt = {name} className="image-card" />
-
-                  
-                <div className="texto-card">
-                
-                <div className="name-card">{name}</div>
-             
-                <ul className="genre-card">{genre.join(", ")}</ul>
-
-                
-                <div className="stars">
-                 { [...Array(Math.floor(rating))].map(( i) => <div key={i}>⭐</div>
-                )}
-                </div>
-                
-                </div>
-
-                   </body>
-                
-
-              
-                
-    );
+            </section>
+    </body>
+  );
 }
